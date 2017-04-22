c414b52ad060a359f66d1f18b48f874dc92e40d2
Rifki Fauzi <kubido@gmail.com>
2017-04-22 18:36:31 +0700

    add vue component for fetching hadits

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
+    <h1>Hello world</h1>
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <div id="hadits-content">
+      <router-view></router-view>
+    </div>
+    <div id="hadits-sidebar">
+      <hadits-sidebar></hadits-sidebar>
+    </div>
   </div>
 </template>
 
---
+++ src/components/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="container">
+    <h1>Hello world</h1>
+    <h2> {{ hadits }} </h2>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default{
+    name: 'index',
+    data () {
+      return {
+        hadits: []
+      }
+    },
+
+    created () {
+      this.fetchHadits()
+    },
+
+    methods: {
+      fetchHadits () {
+        axios.get(`/api/imams/bukhari/hadits.json?page=1&random=true`)
+        .then(function (hadits) {
+          this.hadits = hadits
+        })
+        .catch(e => {
+          alert(e)
+        })
+      }
+    }
+
+  }
+
+</script>
